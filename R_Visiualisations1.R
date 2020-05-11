e48a5af00f5d745793a08d2fd02049468c6a02c4
Imy Younge <imy.younge@sky.com>
2020-05-11 17:58:08 +0100

    Adding code tthat allows access to the data

---
+++ R_Visiualisations1.R
@@ -0,0 +1,5 @@
+# Set_up ------------------------------------------------------------------
+setwd('/Users/imyyounge/Documents/Projects_for_fun/Strava')
+
+#Following tutorial: https://github.com/marcusvolz/strava
+
