e9fbbb51150326c5d804b3d49b5c23f3ebd15abc
Imogen Younge <44430328+Imyyy@users.noreply.github.com>
2025-07-20 15:29:06 +0100

    Initial grouped plot
    Process data and initial plot of distance by activity per year

--- R_visualisations_strava_data/Summary_data_vis.R
+++ R_visualisations_strava_data/Summary_data_vis.R
@@ -1,1 +1,27 @@
+library(dplyr)
+library(ggplot2)
+
 ### Load in summary data
+data <- read.csv("../Data/Processed_data/Totals_by_year.csv")
+
+head(data)
+
+data <- subset(data, select = -c(X))
+
+
+# Filter to only the outdoor
+outdoor <- data %>% 
+  filter(Type!="StairStepper" & Type!="VirtualRide") %>% 
+  rename(Distance.km = Distance..km.)
+
+# Plots -------------------------------------------------------------------
+
+max_dist = round(max(outdoor$Distance.km), -2). # Max dist to nearest 100, for neat plots
+
+ggplot(outdoor, aes(x=Year, y = Distance.km, fill=Type)) + 
+  geom_col(position="stack") + 
+  scale_fill_brewer(palette = "Pastel1") + 
+  theme_linedraw() + 
+  labs(title = "Distance by activity per year", x = "Year", y = "Distance (km)") +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = max_dist / 200))
+
